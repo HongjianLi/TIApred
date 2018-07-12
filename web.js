96626b334076c06d9c3436804ac19d307e047c8a
Jacky Lee <Jacky@localhost.localdomain>
2018-07-12 18:00:07 +0800

    Added a prototype web.js and index.html

---
+++ web.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+'use strict';
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+const port = 3000;
+app.listen(port);
+console.log('Process %d listening on HTTP port %d in %s mode', process.pid, port, app.settings.env);

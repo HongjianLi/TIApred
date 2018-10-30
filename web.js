3316bc717482521628e152deb263b0a54833fa8e
Hongjian Li <JackyLeeHongJian@Gmail.com>
2018-10-30 22:14:37 +0800

    Added sample code for inserting a new info document

--- web.js
+++ web.js
@@ -1,8 +1,28 @@
 #!/usr/bin/env node
 'use strict';
-var express = require('express');
+const mongodb = require('mongodb');
+const express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
 const port = 4000;
 app.listen(port);
 console.log('Process %d listening on HTTP port %d in %s mode', process.pid, port, app.settings.env);
+/*
+(async() => {
+  const mongoClient = await mongodb.MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }); // poolSize is 5 by default
+  const tiaPred = mongoClient.db('TIApred');
+  const info = tiaPred.collection('info');
+  const doc = {
+    "基线登记": {
+      "基本信息": {
+        "身份证号": "4401053215325325",
+        "姓名": "天生一个丽宝",
+        "民族": "汉",
+      },
+      "发病情况": {},
+      "既往病史": {},
+    },
+  }
+  info.insertOne(doc);
+})();
+*/

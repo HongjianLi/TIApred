78971c4cadab90efddc923a6f1000d585b8ca113
Hongjian Li <JackyLeeHongJian@Gmail.com>
2018-11-13 10:38:59 +0800

    Added MongoDB code to web.js

--- web.js
+++ web.js
@@ -1,28 +1,32 @@
 #!/usr/bin/env node
 'use strict';
-const mongodb = require('mongodb');
-const express = require('express');
-var app = express();
-app.use(express.static(__dirname + '/public'));
-const port = 4000;
-app.listen(port);
-console.log('Process %d listening on HTTP port %d in %s mode', process.pid, port, app.settings.env);
-/*
 (async() => {
+  // Connect to MongoDB.
+  const mongodb = require('mongodb');
   const mongoClient = await mongodb.MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }); // poolSize is 5 by default
   const tiaPred = mongoClient.db('TIApred');
-  const info = tiaPred.collection('info');
-  const doc = {
-    "基线登记": {
-      "基本信息": {
-        "身份证号": "4401053215325325",
-        "姓名": "天生一个丽宝",
-        "民族": "汉",
-      },
-      "发病情况": {},
-      "既往病史": {},
-    },
-  }
-  info.insertOne(doc);
+  const emrColl = tiaPred.collection('EMRcoll');
+  // Initialize the web server.
+  const express = require('express');
+  const bodyParser = require('body-parser');
+  var app = express();
+//  app.use(bodyParser.json({ limit: '1mb', strict: false }));
+  app.use(bodyParser.urlencoded({ limit: '2mb', extended: true })); // https://www.npmjs.com/package/body-parser
+  app.use(express.static(__dirname + '/public'));
+  app.route('/record').get((req, res) => {
+    //  let v = new validator(req.body);
+    const emr = emrColl.findOne({ // http://mongodb.github.io/node-mongodb-native/3.1/api/Collection.html#findOne
+      "姓名": req.body["姓名"],
+    });
+    console.log(emr);
+    // Read DB to get a document with req.body["住院号"]
+  }).post((req, res) => {
+    console.log(req.body);
+//  let v = new validator(req.body);
+    emrColl.insertOne(emr);
+    res.json({}});
+  });
+  const port = 4000;
+  app.listen(port);
+  console.log('Process %d listening on HTTP port %d in %s mode', process.pid, port, app.settings.env);
 })();
-*/

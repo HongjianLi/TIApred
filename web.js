4c304fd8ad33c6077ef254d0987b93435f78bc76
Hongjian Li <JackyLeeHongJian@Gmail.com>
2018-11-14 11:04:54 +0800

    Updated web.js to accept /record GET and POST

--- web.js
+++ web.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 'use strict';
-(async() => {
-  // Connect to MongoDB.
-  const mongodb = require('mongodb');
-  const mongoClient = await mongodb.MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }); // poolSize is 5 by default
+const mongodb = require('mongodb');
+mongodb.MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true }).then((mongoClient) => { // poolSize is 5 by default
   const tiaPred = mongoClient.db('TIApred');
   const emrColl = tiaPred.collection('EMRcoll');
   // Initialize the web server.
@@ -14,19 +12,31 @@
   app.use(bodyParser.urlencoded({ limit: '2mb', extended: true })); // https://www.npmjs.com/package/body-parser
   app.use(express.static(__dirname + '/public'));
   app.route('/record').get((req, res) => {
-    //  let v = new validator(req.body);
-    const emr = emrColl.findOne({ // http://mongodb.github.io/node-mongodb-native/3.1/api/Collection.html#findOne
-      "姓名": req.body["姓名"],
-    });
-    console.log(emr);
-    // Read DB to get a document with req.body["住院号"]
+//  let v = new validator(req.query);
+    if (Object.keys(req.query).length) {
+      emrColl.findOne(req.query).then((emr) => { // http://mongodb.github.io/node-mongodb-native/3.1/api/Collection.html#findOne
+        res.json(emr);
+      });
+    } else {
+      emrColl.find({}, {
+        projection: {
+          '基线登记.基本信息.姓名': 1,
+          '基线登记.基本信息.住院号': 1,
+          '基线登记.基本信息.住院日期': 1,
+        },
+      }).toArray((err, emrArr) => {
+        res.json(emrArr);
+      });
+    }
   }).post((req, res) => {
-    console.log(req.body);
 //  let v = new validator(req.body);
-    emrColl.insertOne(emr);
-    res.json({}});
+    emrColl.insertOne(req.body).then((commandResult) => {
+      res.json({
+        insertedId: commandResult.insertedId,
+      });
+    });
   });
   const port = 4000;
   app.listen(port);
   console.log('Process %d listening on HTTP port %d in %s mode', process.pid, port, app.settings.env);
-})();
+});

01de5ecb71d28449a49bc84a88ace416b402a529
Hongjian Li <JackyLeeHongJian@Gmail.com>
2018-12-06 15:11:15 +0800

    Updated ["基线登记"]["基本信息"]["住院日期"] to ["基线登记"]["发病情况"]["到院时间"]

--- public/index.js
+++ public/index.js
@@ -24,7 +24,7 @@ $(function() {
     todayHighlight: true,
     daysOfWeekHighlighted: "0,6",
   });
-//  $('#住院时间').datetimepicker({ // https://eonasdan.github.io/bootstrap-datetimepicker/
+//  $('#到院时间').datetimepicker({ // https://eonasdan.github.io/bootstrap-datetimepicker/
 //    locale: 'zh-CN',
 //  });
 
@@ -60,14 +60,14 @@ $(function() {
       url: "records",
       data: { // Specify the DB query's projection.
         '基线登记.基本信息.住院号': 1,
-        '基线登记.基本信息.住院日期': 1,
+        '基线登记.发病情况.到院时间': 1,
       },
       dataType: "json",
       success: (recordArr, textStatus, jqXHR) => {
         $("#现有记录 option").remove();
         recordArr.forEach((record) => {
           $('#现有记录').append($('<option>', {
-              text: `${record["基线登记"]["基本信息"]["住院日期"]} ${record["基线登记"]["基本信息"]["住院号"]}`,
+              text: `${record["基线登记"]["发病情况"]["到院时间"]} ${record["基线登记"]["基本信息"]["住院号"]}`,
               value: record["基线登记"]["基本信息"]["住院号"],
           }));
         });
